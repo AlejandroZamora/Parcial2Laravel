250930080808dde60be001794ebc80dd3bb8c7b9
Sergio.Lopez <salopez@innovacion.gob.sv>
2025-04-29 20:29:38 -0600

    Fix route definition for home page and comment out old implementation

--- examen2/routes/web.php
+++ examen2/routes/web.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\AlumnosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
 
-Route::get('/alumnos',[AlumnosController::class, 'index']);
+Route::get('/',[AlumnosController::class, 'index']);
 Route::get('/alumnos/eliminar/{id}', [AlumnosController::class, 'destroy']);
 Route::get('/alumnos/editar/{id}', [AlumnosController::class, 'edit']);
 Route::put('/alumnos/editar/{id}', [AlumnosController::class, 'update']);
